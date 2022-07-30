eaa43dfd5262cd591e1ce519fc1d2cec042b6fbc
undefined <shongs27@naver.com>
2022-07-30 23:25:44 +0900

    [Feat] Footerbar 구현

--- src/components/FooterBar.tsx
+++ src/components/FooterBar.tsx
@@ -1,3 +1,10 @@
+import styles from "./footerBar.module.scss";
+
 export default function FooterBar() {
-  return <div>FooterBar</div>;
+  return (
+    <div className={styles.footer}>
+      <p>Design By 홍원배 | 작성자</p>
+      <p>Copyright ⓒ 2022 Hongs All Rights Reserved.</p>
+    </div>
+  );
 }
